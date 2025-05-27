6cc5be6b846ce60e2457a2334c255688186c0f4a
Dangel <an.guillen@duocuc.cl>
2025-05-27 00:53:46 -0400

    first commit

---
+++ src/main/java/com/Habitaciones/Habitacion/Model/Habitacion.java
@@ -0,0 +1,33 @@
+package com.Habitaciones.Habitacion.Model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "habitaciones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Habitacion {
+
+    @Id
+    private int id;
+    private String NumeroHabit; //Numero de la Habitacion
+    private String TipoHabitacion;
+    private int CapacidadMax;
+    //public Cama cama;
+
+    @OneToMany(mappedBy = "habitacion",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Camas> camas;
+}
